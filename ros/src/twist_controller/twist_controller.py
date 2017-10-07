13e7dd0c11a4fbfe0ef5036fba4834c0474d1cda
Eric Lavigne <lavigne.eric@gmail.com>
2017-10-07 09:15:00 -0700

    twist_controller uses two PID controllers for throttle and steering

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,34 +1,36 @@
 import rospy
 from math import atan2, pi, sqrt
+from pid import PID
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        pass
+        self.steer_pid = PID(1., 0., 0., -1., 1.)
+        self.throttle_pid = PID(1., 0., 0., -1., 1.)
 
     def control(self, goal_velocity, goal_angular_velocity, current_velocity, dbw_enabled):
-        throttle = 1.
-        brake = 0.
+        deltat = 0.02
         
-        steer = 0.
-        yaw_diff = 0.
         current_speed = sqrt(current_velocity[0]**2 + current_velocity[1]**2)
         goal_speed = sqrt(goal_velocity[0]**2 + goal_velocity[1]**2)
-        if current_speed > 1 and goal_speed > 1:
-            goal_yaw = atan2(goal_velocity[1],goal_velocity[0])
-            current_yaw = atan2(current_velocity[1],current_velocity[0])
-            yaw_diff = goal_yaw - current_yaw
-            if yaw_diff > pi:
-                yaw_diff = yaw_diff - 2 * pi
-            if yaw_diff < - pi:
-                yaw_diff = yaw_diff + 2 * pi
-            if yaw_diff > 0.1:
-                steer = 1.
-            if yaw_diff < -0.1:
-                steer = -1.
-            #rospy.logwarn("steer: %s   yawdiff: %s   goalspeed: %s   speed: %s",
-            #              steer, yaw_diff, goal_speed, current_speed)
+        speed_diff = goal_speed - current_speed
+        throttle = self.throttle_pid.step(speed_diff, deltat)
+        
+        steer = self.steer_pid.step(goal_angular_velocity, deltat)
+        
+        if dbw_enabled:
+            self.throttle_pid.reset()
+            self.steer_pid.reset()
+        
+        brake = 0.
+        if throttle < 0.:
+            brake = 0. - throttle
+            throttle = 0.
+        
+        #rospy.logwarn("twist_controller | speed_diff: %s   throttle: %s   brake: %s   angular: %s   steer: %s",
+        #      speed_diff, throttle, brake, goal_angular_velocity, steer)
+        
         return throttle, brake, steer
 
