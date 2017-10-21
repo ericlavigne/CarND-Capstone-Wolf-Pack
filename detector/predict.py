ce2ede47639f48e49501fee55beb40e6bdaff9b7
Alena Kastsiukavets <kastsiukavets.alena@gmail.com>
2017-10-21 19:07:57 +0300

    refactor: tl detector

---
+++ detector/predict.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+import argparse
+import glob
+
+from skimage.io import imsave, imread
+import tensorflow as tf
+
+from helpers import print_heading
+from model import preprocess, get_unet
+
+miss = 100
+
+def predict(folder_name):
+    print_heading('Loading and preprocessing data...')
+
+    image_rows = 600
+    image_cols = 800
+    folder_pattern = os.path.join(folder_name, '*.jpg')
+    image_list = glob.glob(folder_pattern)
+
+    total = int(len(image_list)/miss) + 1 if miss > 0 else len(image_list)
+    imgs = np.ndarray((total, image_rows, image_cols, 3), dtype=np.uint8)
+    imgs_id = np.ndarray((total,), dtype=object)
+
+    count = 0
+    for image_path in image_list:
+        image_name = image_path.split('\\')[-1]
+        if miss == 0 or count % miss == 1:
+            index = int(count / miss) if miss > 0 else count
+            imgs[index] = np.array([imread(os.path.join(folder_name, image_name))])
+            imgs_id[index] = image_name.split('.')[0]
+        count += 1
+
+    imgs = preprocess(imgs).astype('float32')
+
+    mean = np.mean(imgs)  # mean for data centering
+    std = np.std(imgs)  # std for data normalization
+    imgs -= mean
+    imgs /= std
+
+    print_heading('Loading saved weights...')
+
+    model = get_unet()
+    model.load_weights('tl_weights.h5')
+
+    print_heading('Predicting masks on test data...')
+
+    imgs_mask = model.predict(imgs, verbose=1)
+
+    print_heading('Saving predicted masks to files...')
+
+    pred_dir = os.path.join(folder_name, 'preds')
+    if not os.path.exists(pred_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask, imgs_id):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(pred_dir, image_id + '.pred.png'), image)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'folder',
+        type=str,
+        help='Path folder with images to predict'
+    )
+    args = parser.parse_args()
+
+    predict(args.folder)
---
+++ detector/train.py
@@ -0,0 +1,73 @@
+import numpy as np
+import os
+
+from skimage.io import imsave, imread
+
+from keras.callbacks import ModelCheckpoint
+
+from helpers import print_heading
+from data import load_train_data, load_test_data
+from model import preprocess, get_unet
+
+
+
+def train_and_predict():
+    print_heading('Loading and preprocessing train data...')
+
+    imgs_train, imgs_mask_train = load_train_data()
+
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    print_heading('Creating and compiling model...')
+
+    model = get_unet()
+    model_checkpoint = ModelCheckpoint('tl_weights.h5', monitor='val_loss', save_best_only=True)
+
+    print_heading('Fitting model...')
+
+    model.fit(imgs_train, imgs_mask_train, batch_size=8, epochs=1, verbose=1, shuffle=True,
+              validation_split=0.2,
+              callbacks=[model_checkpoint])
+
+    print_heading('Fitting model finished')
+
+    print_heading('Loading and preprocessing test data...')
+
+    imgs_test, imgs_id_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
+
+    print_heading('Loading saved weights...')
+
+    model.load_weights('tl_weights.h5')
+
+    print_heading('Predicting masks on test data...')
+
+    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    np.save('imgs_mask_test.npy', imgs_mask_test)
+
+    print_heading('Saving predicted masks to files...')
+
+    pred_dir = 'preds'
+    if not os.path.exists(pred_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask_test, imgs_id_test):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(pred_dir, image_id + '.pred.png'), image)
+
+if __name__ == '__main__':
+    train_and_predict()
