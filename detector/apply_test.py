1186d6410428c646da3e3b50d7e833a4ec7bdd20
Alena Kastsiukavets <kastsiukavets.alena@gmail.com>
2017-11-11 23:48:02 +0300

    fix: update detector model and image extraction

--- detector/apply_test.py
+++ detector/apply_test.py
@@ -19,37 +19,47 @@ col_ratio = image_cols/img_cols
 middle_row = img_rows / 2
 middle_col = img_cols / 2
 
+projection_threshold = 2
+projection_min = 200
+
 root_path = os.path.dirname(os.path.realpath(__file__))
 
 def preprocess(img):
     return resize(img, (img_rows, img_cols), preserve_range=True, mode="constant").astype('float32') / 255;
 
 def extract_image(pred_image_mask, image):
-    column_projection = np.sum(pred_image_mask, axis = 0)
-
-    if (np.max(column_projection) < 20*20):
+    print(np.max(pred_image_mask))
+    if (np.max(pred_image_mask) < projection_min):
         return None
 
-    non_zero_column_index = np.argwhere(column_projection > 20*20)
+    row_projection = np.sum(pred_image_mask, axis = 1)
+    row_index =  np.argmax(row_projection)
 
-    if non_zero_column_index.size == 0:
+    if (np.max(row_projection) < projection_threshold):
         return None
+
+    zero_row_indexes = np.argwhere(row_projection <= projection_threshold)
+    top_part = zero_row_indexes[zero_row_indexes < row_index]
+    top = np.max(top_part) if top_part.size > 0 else 0
+    bottom_part = zero_row_indexes[zero_row_indexes > row_index]
+    bottom = np.min(bottom_part) if bottom_part.size > 0 else img_rows
+
+    roi = pred_image_mask[top:bottom,:]
+    column_projection = np.sum(roi, axis = 0)
+
+    if (np.max(column_projection) < projection_min):
+        return None
+
+    non_zero_column_index = np.argwhere(column_projection > projection_min)
 
     index_of_column_index =  np.argmin(np.abs(non_zero_column_index - middle_col))
     column_index = non_zero_column_index[index_of_column_index][0]
 
     zero_colum_indexes = np.argwhere(column_projection == 0)
-    left = np.max(zero_colum_indexes[zero_colum_indexes < column_index])
-    right = np.min(zero_colum_indexes[zero_colum_indexes > column_index])
-
-    #roi = pred_image_mask[:,left:right]
-
-    row_projection = np.sum(pred_image_mask, axis = 1)
-    row_index =  np.argmax(row_projection)
-
-    zero_row_indexes = np.argwhere(row_projection == 0)
-    top = np.max(zero_row_indexes[zero_row_indexes < row_index])
-    bottom = np.min(zero_row_indexes[zero_row_indexes > row_index])
+    left_side = zero_colum_indexes[zero_colum_indexes < column_index]
+    left = np.max(left_side) if left_side.size > 0 else 0
+    right_side = zero_colum_indexes[zero_colum_indexes > column_index]
+    right = np.min(right_side) if right_side.size > 0 else img_cols
 
     return image[int(top*row_ratio):int(bottom*row_ratio), int(col_ratio*left):int(col_ratio*right)]
 
